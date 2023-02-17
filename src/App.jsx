0b7754c2d752835c86048bcc3286086d66125922
max <bozzhik@ya.ru>
2023-02-17 14:26:48 +0300

    add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <button className="mt-6 rounded border-transparent border-2 bg-[#1a1a1a] text-white text-xl font-medium cursor-pointer transition duration-500 ease-out px-3 py-1 grid place-items-center m-auto" onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="component1">
+      <h1 className="text-2xl text-white">This is my Component1</h1>
+    </div>
+  )
+}
+
+export default Header
