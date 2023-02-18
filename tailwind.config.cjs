ccfbf4fc1fb8b54390f7f2bc04ee620b10270d74
max <bozzhik@ya.ru>
2023-02-18 04:55:32 +0300

    custimize media queries tailwind.config.cjs
    In Tailwind CSS, the classes that are generated for screen sizes are mobile-first. It doesn't suit me 😶

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,6 +21,12 @@ module.exports = {
         custom: ['Space Grotesk', 'ui-sans-serif', 'sans-serif']
       }
     },
+    screens: {
+      sm: { min: '300px', max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px', max: '1279px' },
+      xl: { min: '1280px' }
+    },
     debugScreens: {
       position: ['top', 'right']
     }
