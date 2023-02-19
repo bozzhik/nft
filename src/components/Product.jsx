7f574b928da00f73c7ce2c2b120d0b001c304527
max <bozzhik@ya.ru>
2023-02-19 17:23:05 +0300

    fix img builing && fix #

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,13 @@
+import skull02 from '../assets/nft/skull-02.png'
+import skull01 from '../assets/nft/skull-01.png'
+import skull03 from '../assets/nft/skull-03.png'
+import skull04 from '../assets/nft/skull-04.png'
+import skull05 from '../assets/nft/skull-05.png'
+import skull06 from '../assets/nft/skull-06.png'
+import skull07 from '../assets/nft/skull-07.png'
+import skull08 from '../assets/nft/skull-08.png'
+import skull09 from '../assets/nft/skull-09.png'
+
 import SlideBody from './SlideBody'
 
 export default function Product() {
@@ -6,31 +16,31 @@ export default function Product() {
       <div id="slider" className="min-h-full overflow-hidden swiper">
         <div className="flex max-h-full swiper-wrapper">
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="neon gangster" price="1.325" buttonText="BUY NOW" imageUrl="/src/img/skull-02.png" />
+            <SlideBody caption="neon gangster" price="1.325" buttonText="BUY NOW" imageUrl={skull01} />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="strong samurai" price="1.425" buttonText="BUY NOW" imageUrl="/src/img/skull-01.png" />
+            <SlideBody caption="strong samurai" price="1.425" buttonText="BUY NOW" imageUrl={skull02} />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="banana bandit" price="1.375" buttonText="BUY NOW" imageUrl="/src/img/skull-03.png" />
+            <SlideBody caption="banana bandit" price="1.375" buttonText="BUY NOW" imageUrl={skull03} />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="many-eyed" price="1.215" buttonText="BUY NOW" imageUrl="/src/img/skull-04.png" />
+            <SlideBody caption="many-eyed" price="1.215" buttonText="BUY NOW" imageUrl={skull04} />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="cool baby" price="1.155" buttonText="BUY NOW" imageUrl="/src/img/skull-05.png" />
+            <SlideBody caption="cool baby" price="1.155" buttonText="BUY NOW" imageUrl={skull05} />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="robocop vampire" price="1.105" buttonText="BUY NOW" imageUrl="/src/img/skull-06.png" />
+            <SlideBody caption="robocop vampire" price="1.105" buttonText="BUY NOW" imageUrl={skull06} />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="domsday farmer" price="1.250" buttonText="BUY NOW" imageUrl="/src/img/skull-07.png" />
+            <SlideBody caption="domsday farmer" price="1.250" buttonText="BUY NOW" imageUrl={skull07} />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="sports smoker" price="1.115" buttonText="BUY NOW" imageUrl="/src/img/skull-08.png" />
+            <SlideBody caption="sports smoker" price="1.115" buttonText="BUY NOW" imageUrl={skull08} />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="crazy hipster" price="1.250" buttonText="BUY NOW" imageUrl="/src/img/skull-09.png" />
+            <SlideBody caption="crazy hipster" price="1.250" buttonText="BUY NOW" imageUrl={skull09} />
           </div>
         </div>
       </div>
--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       output: {
         entryFileNames: '[name].[hash].js',
         chunkFileNames: '[name].[hash].js',
-        assetFileNames: '[name].[ext]'
+        assetFileNames: '[name].[hash].[ext]'
       }
     }
   },
