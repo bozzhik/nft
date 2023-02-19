a70766c1c5921a346e90d8fc5dbe9dc3e536a18b
max <bozzhik@ya.ru>
2023-02-19 14:21:37 +0300

    add useRef react hook

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,13 +6,13 @@ export default function Product() {
       <div id="slider" className="min-h-full overflow-hidden swiper">
         <div className="flex max-h-full swiper-wrapper">
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption1="NIKEdd MAG" caption2="BACK TO THE FUTURE" price="749" buttonText="BUY NOW" imageUrl="/src/img/image-01.png" />
+            <SlideBody caption="bearded samurai" price="749" buttonText="BUY NOW" imageUrl="/src/img/skull-01.png" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption1="NsdIKE MAG" caption2="BACK TO THE FUTURE" price="749" buttonText="BUY NOW" imageUrl="/src/img/image-01.png" />
+            <SlideBody caption="neon gangster" price="749" buttonText="BUY NOW" imageUrl="/src/img/skull-02.png" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption1="NIdddsdKE MAG" caption2="BACK TO THE FUTURE" price="749" buttonText="BUY NOW" imageUrl="/src/img/image-01.png" />
+            <SlideBody caption="banana bandit" price="749" buttonText="BUY NOW" imageUrl="/src/img/skull-03.png" />
           </div>
         </div>
       </div>
--- src/components/SlideBody.jsx
+++ src/components/SlideBody.jsx
@@ -1,4 +1,19 @@
-export default function SlideBody({ caption1, caption2, price, buttonText, imageUrl }) {
+import { useRef, useState } from 'react'
+
+export default function SlideBody({ caption, price, buttonText, imageUrl }) {
+  const wrapperRef = useRef(null)
+  const textRef = useRef(null)
+  const [isZIndexLow, setIsZIndexLow] = useState(false)
+
+  const handleClick = () => {
+    if (isZIndexLow) {
+      wrapperRef.current.style.zIndex = '-10'
+    } else {
+      wrapperRef.current.style.zIndex = '10'
+    }
+    setIsZIndexLow(!isZIndexLow)
+  }
+
   return (
     <div className="relative flex flex-col min-h-full px-5 pt-32 pb-5 shrink-0">
       <div className="relative flex items-center justify-center flex-auto">
@@ -7,11 +22,11 @@ export default function SlideBody({ caption1, caption2, price, buttonText, image
             <img className="absolute top-0 left-0 object-contain w-full h-full drop-shadow-custom" src={imageUrl} alt="image" />
           </div>
         </div>
-        <h2 className="absolute w-full h-full top-0 left-0 flex flex-col justify-center text-center items-center z-[2] text-[10vmin] leading-[1.3] uppercase font-bold mix-blend-difference">
-          {caption1}
-          <br />
-          {caption2}
-        </h2>
+        <div ref={wrapperRef} className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full text-center mix-blend-difference" onClick={handleClick} style={{ zIndex: isZIndexLow ? '-2' : '2' }}>
+          <h2 ref={textRef} className="text-[17vmin] leading-[1.3] uppercase font-bold">
+            {caption}
+          </h2>
+        </div>
       </div>
       <div className="mx-auto mt-16 w-full max-w-[990px] flex justify-between gap-5">
         <div className="text-4xl font-medium leading-[1.3]">${price}</div>
