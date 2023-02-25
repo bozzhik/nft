5ec379abe2c1bc7003b9525c49e56c0ea0a03df4
max <bozzhik@ya.ru>
2023-02-25 16:52:30 +0300

    optimize img loading && convert png → webp #

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,3 @@
-import skull02 from '../assets/nft/skull-02.png'
-import skull01 from '../assets/nft/skull-01.png'
-import skull03 from '../assets/nft/skull-03.png'
-import skull04 from '../assets/nft/skull-04.png'
-import skull05 from '../assets/nft/skull-05.png'
-import skull06 from '../assets/nft/skull-06.png'
-import skull07 from '../assets/nft/skull-07.png'
-import skull08 from '../assets/nft/skull-08.png'
-import skull09 from '../assets/nft/skull-09.png'
-
 import SlideBody from './SlideBody'
 
 export default function Slider() {
@@ -16,31 +6,31 @@ export default function Slider() {
       <div id="slider" className="min-h-full overflow-hidden swiper">
         <div className="flex max-h-full swiper-wrapper">
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="strong samurai" price="1.425" buttonText="BUY NOW" imageUrl={skull01} />
+            <SlideBody caption="strong samurai" price="1.425" buttonText="BUY NOW" imageUrl="./src/assets/nft/skull-01.webp" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="neon gangster" price="1.325" buttonText="BUY NOW" imageUrl={skull02} />
+            <SlideBody caption="neon gangster" price="1.325" buttonText="BUY NOW" imageUrl="./src/assets/nft/skull-02.webp" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="banana bandit" price="1.375" buttonText="BUY NOW" imageUrl={skull03} />
+            <SlideBody caption="banana bandit" price="1.375" buttonText="BUY NOW" imageUrl="./src/assets/nft/skull-03.webp" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="many-eyed" price="1.215" buttonText="BUY NOW" imageUrl={skull04} />
+            <SlideBody caption="many-eyed" price="1.215" buttonText="BUY NOW" imageUrl="./src/assets/nft/skull-04.webp" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="cool baby" price="1.155" buttonText="BUY NOW" imageUrl={skull05} />
+            <SlideBody caption="cool baby" price="1.155" buttonText="BUY NOW" imageUrl="./src/assets/nft/skull-05.webp" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="robocop vampire" price="1.105" buttonText="BUY NOW" imageUrl={skull06} />
+            <SlideBody caption="robocop vampire" price="1.105" buttonText="BUY NOW" imageUrl="./src/assets/nft/skull-06.webp" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="domsday farmer" price="1.250" buttonText="BUY NOW" imageUrl={skull07} />
+            <SlideBody caption="domsday farmer" price="1.250" buttonText="BUY NOW" imageUrl="./src/assets/nft/skull-07.webp" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="sports smoker" price="1.115" buttonText="BUY NOW" imageUrl={skull08} />
+            <SlideBody caption="sports smoker" price="1.115" buttonText="BUY NOW" imageUrl="./src/assets/nft/skull-08.webp" />
           </div>
           <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-            <SlideBody caption="crazy hipster" price="1.250" buttonText="BUY NOW" imageUrl={skull09} />
+            <SlideBody caption="crazy hipster" price="1.250" buttonText="BUY NOW" imageUrl="./src/assets/nft/skull-09.webp" />
           </div>
         </div>
       </div>
