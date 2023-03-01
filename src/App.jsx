2d55ffd87389adcce0d4333bff37a1ba8e8f428d
max <bozzhik@ya.ru>
2023-03-01 03:18:24 +0300

    [hard-reset] init default slider with swiper.js
    damn I finally solved the problem with displaying the site on a mobile device. should have read the swiper.js documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,69 @@
-import Header from './components/Header'
-import Product from './components/Product'
+import React, {useRef, useState} from 'react'
+// Import Swiper React components
+import {Swiper, SwiperSlide} from 'swiper/react'
 
-function App() {
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/scrollbar'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+// import required modules
+import {Keyboard, Scrollbar, Navigation, Pagination} from 'swiper'
+
+export default function App() {
   return (
-    // <div className={`bg-${color}-500 text-white font-bold`}></div>
-    <div className={`bg-gradient-to-r from-gradient-start to-gradient-stop fixed top-0 left-0 flex flex-col w-full h-full overflow-auto`}>
-      <Header />
-      <Product />
-    </div>
+    <>
+      <Swiper
+        slidesPerView={1}
+        centeredSlides={false}
+        slidesPerGroupSkip={1}
+        grabCursor={true}
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          769: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+          },
+        }}
+        scrollbar={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://cdn.magloft.com/github/swiper/images/page-001.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.magloft.com/github/swiper/images/page-002.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.magloft.com/github/swiper/images/page-003.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.magloft.com/github/swiper/images/page-004.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.magloft.com/github/swiper/images/page-005.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.magloft.com/github/swiper/images/page-006.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.magloft.com/github/swiper/images/page-007.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.magloft.com/github/swiper/images/page-008.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://cdn.magloft.com/github/swiper/images/page-009.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </>
   )
 }
-
-export default App
