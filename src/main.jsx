1279762d1a1891eb26bb88842f8bd01c2c7e61d7
max <bozzhik@ya.ru>
2023-03-02 00:42:08 +0300

    fix build 7.0+ && fixed mobile website access
    omg I can't even believe I've solved this problem. my hypothesis is about supporting css code, i just splited code for each platform and imported it separately! yes i did it 0.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+if (window.matchMedia('(max-width: 768px)').matches) {
+  import('./mobile.css')
+} else {
+  import('./index.css')
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
