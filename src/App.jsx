0e97457461f21b09daa7b80bfcb1908c58a88fd8
max <bozzhik@ya.ru>
2023-03-01 19:30:20 +0300

    done slider

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import slides from './assets/nft.json'
 
 export default function App() {
   return (
-    <div id="App">
+    <div id="App" className={`fixed top-0 left-0 flex flex-col w-full h-full overflow-auto WRAPPER`}>
       <Header />
       <Slider slides={slides} />
     </div>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,58 +1,35 @@
-import SlideBody from './SlideBody'
-
 import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css'
-import {Keyboard, Scrollbar, Navigation, Pagination} from 'swiper'
 
 export const Slider = ({slides}) => {
   return (
-    <Swiper spaceBetween={50} slidesPerView={2} onSlideChange={() => console.log('slide change')} onSwiper={(swiper) => console.log(swiper)}>
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.image}>
-          <img src={slide.image} alt={slide.caption} />
-        </SwiperSlide>
-      ))}
-      <SwiperSlide className="text-black">Slide 1</SwiperSlide>
-      <SwiperSlide className="text-black">Slide 2</SwiperSlide>
-      <SwiperSlide className="text-black">Slide 3</SwiperSlide>
-      <SwiperSlide className="text-black">Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+    <div>
+      <Swiper spaceBetween={100} slidesPerView={1} className="min-h-full overflow-hidden" onSlideChange={() => console.log('slide change')} onSwiper={(swiper) => console.log(swiper)}>
+        {slides.map((slide) => (
+          <SwiperSlide key={slide.image} className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
+            <div className="relative flex flex-col min-h-full px-5 pt-32 pb-5 shrink-0 sm:min-h-screen sm:px-0 sm:pt-16 SLIDE">
+              <div className="relative flex items-center justify-center flex-auto BODY">
+                <div className="w-[75vh] max-w-[90%] mx-auto my-0 flex sm:w-[100vh] sm:max-w-[100%] CONTENT">
+                  <div className="flex-auto relative pb-[90%]" data-swiper-parallax-opacity="0.5" data-swiper-parallax="0%" data-swiper-parallax-scale="0.3">
+                    <img className="absolute top-0 left-0 object-contain w-full h-full drop-shadow-custom PICTURE" src={slide.image} alt="image" />
+                  </div>
+                </div>
+                <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full text-center mix-blend-difference z-[-2]">
+                  <h2 className="text-[20vmin] leading-[1.3] uppercase font-bold sm:text-[18vmin] sm:mt-16 sm:leading-[5]" data-swiper-parallax-opacity="0.5" data-swiper-parallax="0%" data-swiper-parallax-scale="0.4">
+                    {slide.caption}
+                  </h2>
+                </div>
+              </div>
+              <div className="mx-auto mt-16 w-full max-w-[990px] flex justify-between gap-5" data-swiper-parallax-opacity="0" data-swiper-parallax-scale="0.3">
+                <div className="text-4xl font-medium leading-[1.3] sm:text-2xl PRICE">{slide.price} ETH</div>
+                <button className="flex-initial text-4xl uppercase underline leading-[1.3] font-medium hover:no-underline sm:text-2xl BUY" type="button">
+                  {slide.buttonText}
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   )
 }
-
-// return (
-//   <div className="flex flex-col flex-auto">
-//     <div id="slider" className="min-h-full overflow-hidden swiper">
-//       <div className="flex max-h-full swiper-wrapper">
-//         <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-//           <SlideBody caption="strong samurai" price="1.425" buttonText="BUY NOW" imageUrl="/nft/skull-01.webp" />
-//         </div>
-//         <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-//           <SlideBody caption="neon gangster" price="1.325" buttonText="BUY NOW" imageUrl="/nft/skull-02.webp" />
-//         </div>
-//         <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-//           <SlideBody caption="banana bandit" price="1.375" buttonText="BUY NOW" imageUrl="/nft/skull-03.webp" />
-//         </div>
-//         <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-//           <SlideBody caption="many-eyed" price="1.215" buttonText="BUY NOW" imageUrl="/nft/skull-04.webp" />
-//         </div>
-//         <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-//           <SlideBody caption="cool baby" price="1.155" buttonText="BUY NOW" imageUrl="/nft/skull-05.webp" />
-//         </div>
-//         <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-//           <SlideBody caption="robocop vampire" price="1.105" buttonText="BUY NOW" imageUrl="/nft/skull-06.webp" />
-//         </div>
-//         <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-//           <SlideBody caption="domsday farmer" price="1.250" buttonText="BUY NOW" imageUrl="/nft/skull-07.webp" />
-//         </div>
-//         <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-//           <SlideBody caption="sports smoker" price="1.115" buttonText="BUY NOW" imageUrl="/nft/skull-08.webp" />
-//         </div>
-//         <div className="relative flex flex-col min-h-full px-5 pt-0 pb-5 shrink-0 swiper-slide">
-//           <SlideBody caption="crazy hipster" price="1.250" buttonText="BUY NOW" imageUrl="/nft/skull-09.webp" />
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// )
