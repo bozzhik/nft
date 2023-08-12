b100c2e73efe159a05d3d73e5377e395dbdea845
max <bozzhik@ya.ru>
2023-08-12 16:27:16 +0300

    refactor `react code`

---
+++ src/components/bgGradients.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export const bgGradients = ['linear-gradient(to right, #312093, #8ea8ff)',
+                       'linear-gradient(to right, #702093, #ca8eff)',
+                       'linear-gradient(to right, #d7a210, #fff48e)',
+                       'linear-gradient(to right, #1a1a1a, #494949)',
+                       'linear-gradient(to right, #a300cc, #e6b3ff)',
+                       'linear-gradient(to right, #ff8200, #ffcc00)',
+                       'linear-gradient(to right, #95ff00, #00ff5e)',
+                       'linear-gradient(to right, #1f3481, #3383b6)',
+                       'linear-gradient(to right, #ff00c8, #d159c3)']
