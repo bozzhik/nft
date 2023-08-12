42e8babf60f14c0b3d1d2a7dc0f1edeaef4bdf9b
max <bozzhik@ya.ru>
2023-08-12 16:58:01 +0300

    fix `addToCart.js` on mobile

--- src/javascript/addToCart.js
+++ src/javascript/addToCart.js
@@ -1,25 +1,33 @@
 export function addToCart() {
-  const cart = document.querySelector('.CART')
-  const cartValue = document.querySelector('.CART span')
   const speedAnimation = 1000
+  const cartValue = document.querySelector('.CART span')
+
   document.addEventListener('click', function (e) {
     const targetElement = e.target
     const productSlide = targetElement.closest('.SLIDE')
+
+    if (!productSlide) return
+
     const productImage = productSlide.querySelector('.PICTURE')
     const productImageFly = productImage.cloneNode(true)
+
+    const cart = document.querySelector('.CART') // Moved this line inside the click event handler
     const cartPos = {
       left: cart.getBoundingClientRect().left,
       top: cart.getBoundingClientRect().top,
     }
+
     productImageFly.style.cssText = `
-          position: 'fixed';
-          left: ${productImage.getBoundingClientRect().left}px;
-          top: ${productImage.getBoundingClientRect().top}px;
-          width: ${productImage.offsetWidth}px;
-          height: ${productImage.offsetHeight}px;
-          transition: all ${speedAnimation}ms ease;
-          `
+      position: 'fixed';
+      left: ${productImage.getBoundingClientRect().left}px;
+      top: ${productImage.getBoundingClientRect().top}px;
+      width: ${productImage.offsetWidth}px;
+      height: ${productImage.offsetHeight}px;
+      transition: all ${speedAnimation}ms ease;
+    `
+
     document.body.append(productImageFly)
+
     requestAnimationFrame(() => {
       productImageFly.style.left = `${cartPos.left}px`
       productImageFly.style.top = `${cartPos.top}px`
